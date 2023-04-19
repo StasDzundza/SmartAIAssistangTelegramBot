d57e5e3b7bbdf13933faad8c545847bec399ccfe
Stanislav Dzundza <stas.dzundza@gmail.com>
2023-04-19 23:56:33 +0300

    add constant messages and fields

--- bot.py
+++ bot.py
@@ -32,24 +32,24 @@ class ChatGPTBot:
         self._application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), self._message_handler))
 
     def _openai_api_key_provided(self, context: ContextTypes.DEFAULT_TYPE) -> bool:
-        return "api_key" in context.user_data
+        return constants.API_KEY_FIELD in context.user_data
 
     async def _start_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text("Welcome to the ChatGPT Telegram bot! Set your OpenAI API key with /apikey YOUR_API_KEY command.")
+        await update.message.reply_text(constants.WELCOME_NEW_USER_MESSAGE)
 
     async def _set_api_key_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         api_key = " ".join(context.args)
         if not api_key:
-            await update.message.reply_text("Please provide an API key: /apikey YOUR_API_KEY")
+            await update.message.reply_text(f"{constants.SOMETHING_WENT_WRONG_MESSAGE} {constants.API_KEY_REQUEST_MESSAGE}")
         else:
-            context.user_data["api_key"] = api_key
+            context.user_data[constants.API_KEY_FIELD] = api_key
             await update.message.reply_text("API key set successfully!")
 
     async def _message_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         if self._openai_api_key_provided(context):
             await update.message.reply_text("You entered: " + update.message.text)
         else:
-            await update.message.reply_text("Please set your OpenAI API key first using /apikey command.")
+            await update.message.reply_text(constants.API_KEY_REQUEST_MESSAGE)
 
 
 if __name__ == "__main__":
--- constants.py
+++ constants.py
@@ -1,1 +1,10 @@
+# environment variables
 TELEGRAM_BOT_TOKEN_ENV = "TELEGRAM_BOT_API_KEY"
+
+# telegram fields keys
+API_KEY_FIELD = "api_key"
+
+# predefinded messages
+SOMETHING_WENT_WRONG_MESSAGE = "Something went wrong."
+API_KEY_REQUEST_MESSAGE = "Please set your OpenAI API key with /apikey YOUR_API_KEY command in order to use bot functionality."
+WELCOME_NEW_USER_MESSAGE = "Welcome to the ChatGPT Telegram bot! " + API_KEY_REQUEST_MESSAGE
