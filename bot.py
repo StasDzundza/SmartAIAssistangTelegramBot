00ce64d0ba7bf705c1a8a847d045a684c068eb2a
Stanislav Dzundza <stas.dzundza@gmail.com>
2023-04-19 23:47:36 +0300

    make bot working, add async requests

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 import constants
 from OpenAIClients.ChatGPT.chat_gpt_client import ChatGPTClient
 from telegram import Update
-from telegram.ext import filters, Updater, CommandHandler, MessageHandler, CallbackContext, Defaults
+from telegram.ext import filters, ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, CallbackContext, Defaults
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -16,41 +16,43 @@ logging.basicConfig(
 
 class ChatGPTBot:
     def __init__(self, token):
-        self.updater = Updater(token)
-        self.configure_handlers()
+        self._application = ApplicationBuilder().token(token).build()
+        self._configure_handlers()
 
-    def configure_handlers(self):
-        dispatcher = self.updater.dispatcher
-        dispatcher.add_handler(CommandHandler("start", self.start_handler))
-        dispatcher.add_handler(CommandHandler("apikey", self.set_api_key_handler))
-        dispatcher.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), self.message_handler))
-        
-    def start_handler(self, update: Update, context: CallbackContext):
-        update.message.reply_text("Welcome to the ChatGPT Telegram bot! Set your OpenAI API key with /apikey YOUR_API_KEY command.")
-
-    def set_api_key_handler(self, update: Update, context: CallbackContext):
-        api_key = " ".join(context.args)
-        if not api_key:
-            update.message.reply_text("Please provide an API key: /apikey YOUR_API_KEY")
-        else:
-            context.user_data["api_key"] = api_key
-            update.message.reply_text("API key set successfully!")
-
-    def message_handler(self, update: Update, context: CallbackContext):
-        api_key = context.user_data.get("api_key")
-        if not api_key:
-            update.message.reply_text("Please set your OpenAI API key first using /apikey command.")
-            return
-        update.message.reply_text("You entered: " + update.message.text)
+    def __del__(self):
+        logging.info("Bot ended polling updates")
 
     def run(self):
-        self.updater.start_polling()
+        logging.info("Bot started polling updates")
+        self._application.run_polling()
 
-    def stop(self):
-        self.updater.stop()
+    def _configure_handlers(self):
+        self._application.add_handler(CommandHandler("start", self._start_handler))
+        self._application.add_handler(CommandHandler("apikey", self._set_api_key_handler))
+        self._application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), self._message_handler))
+
+    def _openai_api_key_provided(self, context: ContextTypes.DEFAULT_TYPE) -> bool:
+        return "api_key" in context.user_data
+
+    async def _start_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("Welcome to the ChatGPT Telegram bot! Set your OpenAI API key with /apikey YOUR_API_KEY command.")
+
+    async def _set_api_key_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        api_key = " ".join(context.args)
+        if not api_key:
+            await update.message.reply_text("Please provide an API key: /apikey YOUR_API_KEY")
+        else:
+            context.user_data["api_key"] = api_key
+            await update.message.reply_text("API key set successfully!")
+
+    async def _message_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        if self._openai_api_key_provided(context):
+            await update.message.reply_text("You entered: " + update.message.text)
+        else:
+            await update.message.reply_text("Please set your OpenAI API key first using /apikey command.")
 
 
 if __name__ == "__main__":
-    token = os.getenv(constants.TELEGRAM_BOT_API_KEY_ENV)
-    bot = ChatGPTBot(token)
+    bot_token = os.getenv(constants.TELEGRAM_BOT_TOKEN_ENV)
+    bot = ChatGPTBot(bot_token)
     bot.run()
